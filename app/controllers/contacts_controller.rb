4cb6f22aa0fd2cd8a8d355125d0cf6e3818b8fc2
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-08-29 21:00:18 -0500

    added Bootstrap

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,4 +6,9 @@ def contact_action
   render 'contact_page.html.erb'
 end
 
+def all_contacts_action
+  @contacts = Contact.all
+  render 'all_contacts_page.html.erb'
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/contact_url' => 'contacts#contact_action'
+  get 'all_contacts_url' => 'contacts#all_contacts_action'
 end
